7a84899627daac82412f8d9b4243613167d8c2e8
James Lockhart <james@n3tw0rk.co.uk>
2016-10-20 01:41:50 +0100

    Created initial structure

---
+++ replacer/src/main/groovy/uk/co/n3tw0rk/gradle/maven/replacer/ReplacerPlugin.groovy
@@ -0,0 +1,11 @@
+package uk.co.n3tw0rk.gradle.maven.replacer
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ReplacerPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
---
+++ replacer/src/main/groovy/uk/co/n3tw0rk/gradle/maven/replacer/ReplacerTask.groovy
@@ -0,0 +1,11 @@
+package uk.co.n3tw0rk.gradle.maven.replacer
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class ReplacerTask extends DefaultTask {
+    @TaskAction
+    def replacer() {
+
+    }
+}
